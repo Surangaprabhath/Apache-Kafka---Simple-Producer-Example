dbdb8ec552ab377fb190aa786190d802772ff4b2
Suranga Prabhath <37041843+Surangaprabhath@users.noreply.github.com>
2021-07-25 20:40:06 +0530

    Update SimpleConsumer.java

--- SimpleConsumer.java
+++ SimpleConsumer.java
@@ -20,9 +20,9 @@ public class SimpleConsumer {
       props.put("auto.commit.interval.ms", "1000");
       props.put("session.timeout.ms", "30000");
       props.put("key.deserializer", 
-         "org.apache.kafka.common.serializa-tion.StringDeserializer");
+         "org.apache.kafka.common.serialization.StringDeserializer");
       props.put("value.deserializer", 
-         "org.apache.kafka.common.serializa-tion.StringDeserializer");
+         "org.apache.kafka.common.serialization.StringDeserializer");
       KafkaConsumer<String, String> consumer = new KafkaConsumer
          <String, String>(props);
       
